8869a67edc63552fcea92c8a9e1370c20b32e80a
Felipe Ramirez <framirez.d85@gmail.com>
2021-09-25 23:23:34 -0300

    Creacion Mantenedor Usuarios Front

--- SistemaBuscador.Test/PruebasUnitarias/Controladores/LoginControllerTest.cs
+++ SistemaBuscador.Test/PruebasUnitarias/Controladores/LoginControllerTest.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using SistemaBuscador.Controllers;
 using SistemaBuscador.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using SistemaBuscador.Repositories;
 using System.Threading.Tasks;
 
 namespace SistemaBuscador.Test.PruebasUnitarias.Controladores
@@ -17,7 +16,7 @@ namespace SistemaBuscador.Test.PruebasUnitarias.Controladores
         {
             //Preparacion
             var loginRepository = new LoginRepositoryEFFalse();
-            var model = new LoginVIewModel() {Usuario="",Password="" };
+            var model = new LoginVIewModel() { Usuario = "", Password = "" };
             //Ejecucion
             var controller = new LoginController(loginRepository);
             controller.ModelState.AddModelError(string.Empty, "Datos invalidos");
@@ -32,10 +31,17 @@ namespace SistemaBuscador.Test.PruebasUnitarias.Controladores
         public async Task LoginUsuarioNoExiste()
         {
             //Preparacion
-            var loginService = new LoginRepositoryEFFalse();
+            //var loginService = new LoginRepositoryEFFalse();
+            var loginService = new Mock<ILoginRepository>();
+            loginService.Setup(x =>
+            x.UserExist(
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+                .Returns(Task.FromResult(false));
+
             var model = new LoginVIewModel() { Usuario = "Usuario1", Password = "Password1" };
             //Ejecucion
-            var controller = new LoginController(loginService);
+            var controller = new LoginController(loginService.Object);
             var resultado = await controller.Login(model) as ViewResult;
             //validacion
             Assert.AreEqual(resultado.ViewName, "Index");
@@ -45,10 +51,17 @@ namespace SistemaBuscador.Test.PruebasUnitarias.Controladores
         public async Task LoginUsuarioExiste()
         {
             //Preparacion
-            var loginService = new LoginRepositoryEFTrue();
+            //var loginService = new LoginRepositoryEFTrue();
+            var loginService = new Mock<ILoginRepository>();
+            loginService.Setup(x =>
+            x.UserExist(
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+                .Returns(Task.FromResult(true));
+
             var model = new LoginVIewModel() { Usuario = "Usuario1", Password = "Password1" };
             //Ejecucion
-            var controller = new LoginController(loginService);
+            var controller = new LoginController(loginService.Object);
             var resultado = await controller.Login(model) as RedirectToActionResult;
             //validacion
             Assert.AreEqual(resultado.ActionName, "Index");
---
+++ SistemaBuscador/Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaBuscador.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NuevoUsuario()
+        {
+            return View();
+
+        }
+    }
+
+}
